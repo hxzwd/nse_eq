81344b0f7716eb6e2f8e59193c1b89a75a0b4dd5
hxzwd <omnomnomrus43@gmail.com>
2020-02-14 12:07:29 -0500

    Fri Feb 14 12:07:29 EST 2020

--- wl_main/make_report.sh
+++ wl_main/make_report.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+TEX_FILE=$1
+
+
 echo "MAKE REPORT"
-echo
+echo "TEX_FILE = $TEX_FILE"
+
 
 cd reports
 
-echo -e "\n\n\n\n\n" | pdflatex report.tex
+#echo -e "\n\n\n\n\n" | pdflatex report.tex
+echo -e "\n\n\n\n\n" | pdflatex $TEX_FILE
 
 cd ../
 
